6b1ed89aa9dc54d4b2280b9b641bda73898f3696
Syed Mohsin Abbas Naqvi <naqvisyed9221@gmail.com>
2025-01-13 00:14:55 +0500

    first commit

---
+++ frontend/src/Courses/Courses.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Navbar from '../component/Navbar'
+import Footer from '../component/Footer'
+import Course from '../component/Course'
+
+
+function Courses() {
+ 
+  return (
+    <>
+    <Navbar/>
+   
+    <Course/>
+    
+    <Footer/>
+    </>
+  )
+}
+
+export default Courses
